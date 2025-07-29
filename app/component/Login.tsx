cd624bc8ee128f7bada730861cc723cd7e638a73
tenkus47 <tenkus47@gmail.com>
2025-07-29 11:14:00 +0530

    refactor entry.client.tsx to simplify hydrateRoot and update Login component for production URL

--- app/component/Login.tsx
+++ app/component/Login.tsx
@@ -1,18 +1,8 @@
 import { useLoaderData } from "@remix-run/react";
 import { useAuth0 } from "./hook/useAuth";
-import { useRef, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "~/shadComponent/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/shadComponent/ui/dialog";
-import { twMerge } from "tailwind-merge";
+
 // If the user lands on this page, we redirect back to / if they are already logged in.
 
 // This form would take us to the auth0 route, which would redirect to the Auth0 login page.
@@ -21,7 +11,9 @@ export default function Login() {
   let { env } = useLoaderData();
   let { AUTH0_DOMAIN, AUTH0_CLIENT_ID, NODE_ENV } = env;
   let location =
-    NODE_ENV === "production" ? "https://pecha.tools" : "http://localhost:3000";
+    NODE_ENV === "production"
+      ? "https://tools.monlam.ai"
+      : "http://localhost:3000";
   const auth0Config = {
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
@@ -30,22 +22,20 @@ export default function Login() {
     scope: "email profile openid",
   };
 
-  const { loginWithGoogle} =
-    useAuth0(auth0Config);
-  
+  const { loginWithGoogle } = useAuth0(auth0Config);
 
   // Login with Google
   const handleGoogleLogin = () => {
     loginWithGoogle();
   };
- 
+
   return (
-          <Button
-            onClick={handleGoogleLogin}
-            className="text-white w-fit mx-auto bg-blue-600 hover:bg-blue-700 "
-          >
-            <FcGoogle className="text-xl" />
-            <span className="text-lg font-semibold leading-6 ml-3">Google</span>
-          </Button>
+    <Button
+      onClick={handleGoogleLogin}
+      className="text-white w-fit mx-auto bg-blue-600 hover:bg-blue-700 "
+    >
+      <FcGoogle className="text-xl" />
+      <span className="text-lg font-semibold leading-6 ml-3">Google</span>
+    </Button>
   );
 }
